1c79d7c4f56e8ba7b6cc98ac062df3484ded0502
philip713 <105677806+philip713@users.noreply.github.com>
2025-06-24 18:48:08 -0600

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Philip Janzel Paradeza on 2025-06-24.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var image: Image?
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            ContentUnavailableView{
+                Label("No snippets", systemImage: "swift")
+            } description: {
+                Text("You dont have any saved snippets yet.")
+            } actions: {
+                Button("Create Snippet"){
+                    
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+        .onAppear(perform: loadImage)
+        .padding()
+        
+    }
+    func loadImage() {
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey){
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey){currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)}
+        
+        guard let outputImage = currentFilter.outputImage else { return}
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Philip Janzel Paradeza on 2025-06-24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
