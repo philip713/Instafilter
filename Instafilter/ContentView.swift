2feca7fc061cd1688d59f03db66650c3d619f72b
philip713 <105677806+philip713@users.noreply.github.com>
2025-07-26 05:21:03 -0600

    basic UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,44 +10,40 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var pickerItem: PhotosPickerItem?
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImage: Image?
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
     var body: some View {
-        VStack {
-            PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
-            selectedImage?
-                .resizable()
-                .scaledToFit()
-        }
-//        .onAppear(perform: loadImage)
-        .onChange(of: pickerItem){
-            Task {
-                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+        NavigationStack{
+            VStack{
+                Spacer()
+                if let processedImage{
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No pic", systemImage: "photo.badge.plus", description: Text("tap to import photo"))
+                }
+                Spacer()
+                HStack{
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                HStack{
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+                    //share
+                }
             }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
     }
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        if inputKeys.contains(kCIInputIntensityKey){
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey){currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)}
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)}
+    func changeFilter(){
         
-        guard let outputImage = currentFilter.outputImage else { return}
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
-        let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
     }
 }
 
