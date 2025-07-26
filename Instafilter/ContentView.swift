be002fb2461863a85de4b34c6a8385c51fec0f0c
philip713 <105677806+philip713@users.noreply.github.com>
2025-07-26 06:22:08 -0600

    photo picker

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -15,17 +15,22 @@ import StoreKit
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     var body: some View {
         NavigationStack{
             VStack{
                 Spacer()
-                if let processedImage{
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No pic", systemImage: "photo.badge.plus", description: Text("tap to import photo"))
+                PhotosPicker(selection: $selectedItem){
+                    if let processedImage{
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No pic", systemImage: "photo.badge.plus", description: Text("tap to import photo"))
+                    }
                 }
+                .onChange(of: selectedItem, loadImage)
+
                 Spacer()
                 HStack{
                     Text("Intensity")
@@ -44,6 +49,13 @@ struct ContentView: View {
     }
     func changeFilter(){
         
+    }
+    func loadImage(){
+        Task{
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {return}
+            guard let inputImage = UIImage(data: imageData) else {return}
+            //erza's
+        }
     }
 }
 
