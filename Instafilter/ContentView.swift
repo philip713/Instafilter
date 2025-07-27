fcb3a6a379d0a0f94122c7c59f72aea9ae821005
philip713 <105677806+philip713@users.noreply.github.com>
2025-07-26 21:01:37 -0600

    different filters

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     @State private var selectedItem: PhotosPickerItem?
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilters = false
+    
+    let context = CIContext()
     var body: some View {
         NavigationStack{
             VStack{
@@ -35,6 +39,7 @@ struct ContentView: View {
                 HStack{
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
                 HStack{
@@ -44,19 +49,47 @@ struct ContentView: View {
                 }
             }
             .padding([.horizontal, .bottom])
+            .confirmationDialog("Select a filter", isPresented: $showingFilters){
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
+            }
             .navigationTitle("Instafilter")
         }
     }
     func changeFilter(){
-        
+        showingFilters = true
     }
     func loadImage(){
         Task{
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {return}
             guard let inputImage = UIImage(data: imageData) else {return}
-            //erza's
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
     }
+    func applyProcessing(){
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey){ currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)}
+        if inputKeys.contains(kCIInputRadiusKey){ currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey){ currentFilter.setValue(filterIntensity*10, forKey: kCIInputScaleKey)}
+        guard let outputImage = currentFilter.outputImage else {return}
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    func setFilter(_ filter: CIFilter){
+        currentFilter = filter
+        loadImage()
+    }
+    
 }
 
 #Preview {
