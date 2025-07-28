6bf465a1ff5fd928f3461dd8166f5b092e071ad7
philip713 <105677806+philip713@users.noreply.github.com>
2025-07-28 02:59:03 -0600

    sliders and filters

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -15,10 +15,13 @@ import StoreKit
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
     @State private var selectedItem: PhotosPickerItem?
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     @State private var showingFilters = false
-    
+    @AppStorage("filterCount") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
     let context = CIContext()
     var body: some View {
         NavigationStack{
@@ -36,16 +39,28 @@ struct ContentView: View {
                 .onChange(of: selectedItem, loadImage)
 
                 Spacer()
-                HStack{
+                HStack(){
                     Text("Intensity")
                     Slider(value: $filterIntensity)
                         .onChange(of: filterIntensity, applyProcessing)
                 }
-                .padding(.vertical)
+                HStack{
+                    Text("Radius")
+                    Slider(value: $filterRadius)
+                        .onChange(of: filterRadius, applyProcessing)
+                }
+                HStack{
+                    Text("Scale")
+                    Slider(value: $filterScale)
+                        .onChange(of: filterScale, applyProcessing)
+                }
                 HStack{
                     Button("Change Filter", action: changeFilter)
                     Spacer()
-                    //share
+                    if let processedImage{
+                        ShareLink(item: processedImage, preview: SharePreview("Unstafilter image", image: processedImage))
+                    }
+                    
                 }
             }
             .padding([.horizontal, .bottom])
@@ -57,6 +72,9 @@ struct ContentView: View {
                 Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
                 Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
                 Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Morphology Gradient") { setFilter(CIFilter.morphologyGradient()) }
+                Button("Kaleidoscope") { setFilter(CIFilter.kaleidoscope()) }
+                Button("Spotlight") { setFilter(CIFilter.spotLight()) }
                 Button("Cancel", role: .cancel) { }
             }
             .navigationTitle("Instafilter")
@@ -78,16 +96,20 @@ struct ContentView: View {
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey){ currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)}
-        if inputKeys.contains(kCIInputRadiusKey){ currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)}
-        if inputKeys.contains(kCIInputScaleKey){ currentFilter.setValue(filterIntensity*10, forKey: kCIInputScaleKey)}
+        if inputKeys.contains(kCIInputRadiusKey){ currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey){ currentFilter.setValue(filterScale*10, forKey: kCIInputScaleKey)}
         guard let outputImage = currentFilter.outputImage else {return}
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
         let uiImage = UIImage(cgImage: cgImage)
         processedImage = Image(uiImage: uiImage)
     }
-    func setFilter(_ filter: CIFilter){
+    @MainActor func setFilter(_ filter: CIFilter){
         currentFilter = filter
         loadImage()
+        filterCount += 1
+        if filterCount >= 5 {
+            requestReview()
+        }
     }
     
 }
